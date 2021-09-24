0187387cbd053b65c7e1137cf75b3384f8027155
eXqusic92 <77026622+eXqusic92@users.noreply.github.com>
2021-09-24 21:39:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>hello world</h1>"
 
 @app.route("/api/v1/hello-world-1")
 def task():
-    return "<h1>hello student</h1>"
+    return "hello yura"
 
 if __name__ == "__main__":
     app.run()
